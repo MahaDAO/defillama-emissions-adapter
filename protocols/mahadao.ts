7f5057d1157430cfc653edb5005fd5b79548166b
Steven Enamakel <enamakel@mahadao.com>
2023-05-06 12:02:02 +0300

    added the first version of mahadao's emission schedule

---
+++ protocols/mahadao.ts
@@ -0,0 +1,43 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1608422400;
+const totalQty = 10000000;
+
+const maha: Protocol = {
+  "Ecosystem Growth": manualCliff(start, totalQty * 0.12),
+  "Treasury / Grants / Partnerships / Bug Bounties": manualCliff(
+    1608426000,
+    totalQty * 0.05
+  ),
+  "Core Team": [
+    manualCliff(1608422400, totalQty * 0.1),
+    manualLinear(1608422400, 1639958400, totalQty * 0.1),
+  ],
+  "Private Investors": [
+    manualCliff(1608422400, totalQty * 12 * 0.02),
+    manualLinear(
+      start,
+      start + periodToSeconds.month * 6,
+      totalQty * 0.12 * 0.05
+    ),
+    manualLinear(
+      start + periodToSeconds.month * 6,
+      start + periodToSeconds.year,
+      totalQty * 0.12 * 0.05
+    ),
+  ],
+  meta: {
+    sources: ["https://docs.mahadao.com/the-maha-token/distribution"],
+    token: "ethereum:0x745407c86df8db893011912d3ab28e68b62e49b0",
+    protocolIds: ["359", "2076", "2121", "2221", "2607"],
+  },
+  sections: {
+    insiders: ["Private Investors", "Team / Founders / Early Project Members"],
+    noncirculating: ["Treasury / Grants / Partnerships / Bug Bounties"],
+    farming: ["Community"],
+  },
+};
+
+export default maha;
